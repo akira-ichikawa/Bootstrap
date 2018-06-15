35dae4d6d04c59806038dc5010b7b619acae4f14
EC2 Default User <ec2-user@ip-172-31-38-143.us-east-2.compute.internal>
2018-06-15 06:59:53 +0000

    validation

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -8,4 +8,8 @@
     
     {!! link_to_route('tasks.edit', 'このタスクを編集', ['id' => $task->id]) !!}
 
+    {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除') !!}
+    {!! Form::close() !!}
+
 @endsection
